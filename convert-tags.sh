ec06bd8d897a33b06ef1ed37f9b94418b04029e8
Björn Ricks <bjoern.ricks@gmail.com>
2017-09-20 14:01:47 +0200

    Convert tags names automatically
    Convert <name>-<version> to v<version>.

--- convert-tags.sh
+++ convert-tags.sh
@@ -4,6 +4,7 @@ git for-each-ref --format='%(refname)' refs/heads/tags |
 cut -d / -f 4 |
 while read ref
 do
-  git tag "$ref" "refs/heads/tags/$ref";
+  SHORT=`echo $ref | sed 's/[a-z]\+-.*-\([0-9].\+\)/\1/'`
+  git tag "v$SHORT" "refs/heads/tags/$ref";
   git branch -D "tags/$ref";
 done
